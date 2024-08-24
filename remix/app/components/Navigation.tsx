074baabf05ce418be9b343ee62246b78c5cddbb8
Leo <leo.hilsheimer@gmail.com>
2024-08-24 15:12:32 +0200

    make pending navigation visible in main navigation

--- remix/app/components/Navigation.tsx
+++ remix/app/components/Navigation.tsx
@@ -1,17 +1,21 @@
 import { NavLink } from "@remix-run/react";
 import { cn } from "~/util/cn";
 
-const linkClassName = ({ isActive }: { isActive: boolean }) =>
+const linkClassName = ({
+  isActive,
+  isPending,
+}: {
+  isActive: boolean;
+  isPending: boolean;
+}) =>
   cn("hover:underline", {
-    underline: isActive,
+    underline: isActive || isPending,
+    "font-bold": isPending,
   });
 
 export const Navigation: React.FC<{ className?: string }> = ({ className }) => (
   <nav className={cn("flex flex-col gap-2", className)}>
-    <NavLink
-      to="/"
-      className={({ isActive }) => cn(linkClassName({ isActive }), "sm:hidden")}
-    >
+    <NavLink to="/" className={(args) => cn(linkClassName(args), "sm:hidden")}>
       start
     </NavLink>
     <NavLink to="/about" className={linkClassName}>
